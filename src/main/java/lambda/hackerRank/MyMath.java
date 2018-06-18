c6ba879d88e197274ad7257bde6084bac372b4e0
Lina Hovanessian <lina.hovanessian@gmail.com>
2018-06-18 15:33:32 +0400

    Lambda example

---
+++ src/main/java/lambda/hackerRank/MyMath.java
@@ -0,0 +1,30 @@
+package lambda.hackerRank;
+
+/**
+ * @author linahovanessian on 6/18/18.
+ */
+public class MyMath {
+    public static boolean checker(PerformOperation f, int a) {
+        return f.checker( a );
+    }
+
+
+    public PerformOperation isOdd() {
+        return (num) -> num % 2 != 0;
+    }
+
+    public PerformOperation isPrime() {
+        return (num) -> {
+            for (int i = 2; i <= num / 2; i++) {
+                if (num % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        };
+    }
+
+    public PerformOperation isPalindrome() {
+        return (num) -> Integer.toString( num ).equals( new StringBuilder( Integer.toString( num ) ).reverse().toString() );
+    }
+}
---
+++ src/main/java/lambda/hackerRank/PerformOperation.java
@@ -0,0 +1,9 @@
+package lambda.hackerRank;
+
+/**
+ * @author linahovanessian on 6/18/18.
+ */
+public interface PerformOperation {
+
+    boolean checker(int a);
+}
